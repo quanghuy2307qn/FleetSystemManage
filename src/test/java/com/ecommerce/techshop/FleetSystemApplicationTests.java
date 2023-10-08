fc270f27afd7f5aab5a3070a8570a45aa7e91576
quanghuy2307qn <huyxomvangdanh@gmail.com>
2023-10-08 12:44:36 +0700

    Fleet System

---
+++ src/test/java/com/ecommerce/techshop/FleetSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.techshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FleetSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
